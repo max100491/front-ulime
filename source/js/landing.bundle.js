f0523f5d89b1df4b5d991cce81d86e70a21b6c8b
alechq <potapchino@gmail.com>
2015-10-23 22:50:09 +0300

    some responsive

--- source/js/landing.bundle.js
+++ source/js/landing.bundle.js
@@ -7,3 +7,14 @@ var animate = require('./partials/customAnimate.js');
 $('html').removeClass('no-js');
 animate();
 ddw();
+
+
+
+
+// sideNav
+var ham = $('.header__hmbrgr');
+var sideNav = $('.header__m-nav');
+
+ham.click(function(){
+	sideNav.toggleClass('active');
+});
