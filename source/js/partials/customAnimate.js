a4462804e5ca3a9bcd0eb4151a51846c782e9bc9
alechq <potapchino@gmail.com>
2015-10-24 21:33:57 +0300

    правки

--- source/js/partials/customAnimate.js
+++ source/js/partials/customAnimate.js
@@ -5,18 +5,19 @@ function animate(){
 	var vh = $(window).height();
 
 	var zeroLineHalf = vh/2;
+	var zeroLineFour = vh - ( vh/4 );
 	var zeroLineThird = vh - ( vh/3 );
 
 	var element1 = $('.feature-box-nest')[0];
 	var element2 = $('.comments')[0];
 
-	if (element1.getBoundingClientRect().top < zeroLineHalf) $(element1).addClass('animate');
-	if (element2.getBoundingClientRect().top < zeroLineHalf) $(element2).addClass('animate');
+	if (element1.getBoundingClientRect().top < zeroLineFour) $(element1).addClass('animate');
+	if (element2.getBoundingClientRect().top < zeroLineFour) $(element2).addClass('animate');
 
 	$(window).scroll(function(){
 
-		if (element1.getBoundingClientRect().top < zeroLineHalf) $(element1).addClass('animate');
-		if (element2.getBoundingClientRect().top < zeroLineHalf) $(element2).addClass('animate');
+		if (element1.getBoundingClientRect().top < zeroLineFour) $(element1).addClass('animate');
+		if (element2.getBoundingClientRect().top < zeroLineFour) $(element2).addClass('animate');
 
 	});
 };
